52d2ab8a93e64259b7c059723d631f6a96893232
Holden Ernest <holden.ernest@icloud.com>
2025-04-14 20:07:10 -0400

    better User Experience

--- app/src/main/java/com/holden/newgames/HangmanGame.kt
+++ app/src/main/java/com/holden/newgames/HangmanGame.kt
@@ -9,6 +9,7 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.log
+import kotlin.random.Random
 
 
 class HangmanGame : AppCompatActivity() {
@@ -17,10 +18,19 @@ class HangmanGame : AppCompatActivity() {
     var word = "newword";
     var guesses = ArrayList<Char>();
 
+    val wordPool = listOf(
+        "ocean", "giraffe", "marble", "twilight", "velvet", "cactus", "wander", "breeze", "echo", "lantern",
+        "ember", "crystal", "shadow", "nectar", "whistle", "drift", "thunder", "petal", "glimmer", "foggy",
+        "meadow", "ripple", "quartz", "solstice", "midnight", "lilac", "hazel", "willow", "feather", "fable",
+        "hollow", "frost", "pebble", "dusk", "gleam", "harbor", "pine", "horizon", "blossom", "raven",
+        "murmur", "sapphire", "storm", "cobweb", "ivy", "galaxy", "wisp", "boulder", "aurora", "bloom"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hangman)
         var guessInput = findViewById<EditText>(R.id.guessInput)
+        setupGame()
         guessInput.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
@@ -29,7 +39,7 @@ class HangmanGame : AppCompatActivity() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
 
                 if (s?.length == 0) return
-                Log.d("sometag", "char: " + s!!.get(0))
+                //Log.d("sometag", "char: " + s!!.get(0))
                 guess(s!!.get(0))
             }
 
@@ -43,9 +53,11 @@ class HangmanGame : AppCompatActivity() {
         wrong = 0
         word = newWord()
         guesses.clear()
+        updateAns()
     }
     fun newWord():String {
-        return "newword"
+        var ran:Int = Random.nextInt(wordPool.size)
+        return wordPool[ran];
     }
 
     fun ansString():String {
@@ -68,14 +80,29 @@ class HangmanGame : AppCompatActivity() {
         var ansText = findViewById<TextView>(R.id.wrongText)
         ansText.text = ("wrong: " + wrong.toString())
     }
+    fun updateWrongList(c:Char) {
+        var ansText = findViewById<TextView>(R.id.wrongTextList)
+        var oldText = ansText.getText()
+        if (wrong > 1)
+            ansText.setText(oldText.toString() + ", " + c)
+        else {
+            ansText.setText(oldText.toString() + c)
+        }
+    }
 
     fun guess(c:Char) {
+        if (c in guesses) {
+            // already made this guess
+            //Log.d("sometag", "already made this guess")
+            return;
+        }
         guesses.add(c)
         if (correctGuess(c)) {
             updateAns()
         } else {
             wrong++
             updateWrong()
+            updateWrongList(c)
         }
     }
 
